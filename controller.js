d63305c718a2f936d9d4209d74d5d3d23b23fc7d
Merry01 <71989313+Merry01@users.noreply.github.com>
2020-11-28 23:50:54 +0700

    Update controller.js

--- controller.js
+++ controller.js
@@ -4,6 +4,6 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Apilkasi REST API ku berjalan!")
+    response.ok("Apilkasi REST API ku berjalan!",res)
 
 };
